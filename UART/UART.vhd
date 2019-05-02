38fbf3627025e94f769afc81a6c9067d5d4a5508
17103308 <17103308@facin.portoalegre.pucrsnet.br>
2019-05-02 17:35:54 -0300

    deu ruim

--- UART/UART.vhd
+++ UART/UART.vhd
@@ -20,20 +20,32 @@ entity UART is
 		inta: in std_logic;
 		intr: out std_logic;
 			-- interface com outra UART
-		TX: out std_logic_vector(11 downto 0);
-		RX: in std_logic_vector(11 downto 0)
-	);
+		TX: out std_logic;
+		sendTX: out std_logic;
+		ackTX: in std_logic;
+		RX: in std_logic;
+		sendRX: in std_logic;
+		ackRX: out std_logic
+		);
 end UART;
 
 architecture UART of UART is
 
-	signal RW: std_logic_vector(7 downto 0);
+	signal RW: std_logic_vector(7 downto 0) := "00000000";
 	signal BASE: std_logic_vector(7 downto 0) := "00000000";
-	signal RR: std_logic_vector(7 downto 0);
-	signal RRI: std_logic_vector(11 downto 0);
-	signal RWO: std_logic_vector(11 downto 0);
+	signal RR: std_logic_vector(7 downto 0) := "00000000";
+	signal RRI: std_logic_vector(11 downto 0) := "000000000000";
+	signal RWO: std_logic_vector(11 downto 0) := "000000000000";
 	
 	signal contador: integer := 0;
+	
+	signal contadorRX: integer := 0;
+	signal dadoRX: std_logic_vector(11 downto 0);
+	
+	signal contadorTX: integer := 0;
+	signal dadoTX: std_logic_vector(11 downto 0);
+	
+	signal novoDado: std_logic := 0;
 
 begin
 
@@ -55,10 +67,53 @@ begin
 	end if;
 end process;
 
+--RECEPÇÃO DE DADOS
+
+process(ck,rst)
+begin
+	if rst = '1' then
+	elsif ck'event and ck = '1' then
+			if contadorRX < 11 then
+				if sendRX = '1' then
+					dadoRX(contador) <= RX;
+					ackRX <= '1';
+				else
+					contadorRX <= contadorRX + 1;
+					ackRX <= 0;
+				end if;
+			else
+				contador <= 0;		
+				RRI <= dadoRX;		
+			end if;
+	end if;
+end process;
+
+--TRANSMISSAO DE DADO
+
+process(ck,rst)
+begin
+
+	if rst = '1' then
+	elsif ck'event and ck = '1' then
+		if contadorTX < 11 then
+			TX <= RWO(contadorTX);
+			sendTX <= '1';
+			if ackTX = '1' then
+				RWO(contadorTX) <= '0';
+				sendTX <= '0';
+				contadorTX <= contadorTX + 1;
+			end if;	
+		else
+			contador <= 0;
+		end if;
+	end if;
+
+end process;
+
 -- PASSAGEM DE RRI PARA RR
 -- lógica de controle dos bits de start, paridade e stop
 
-process
+process()
 begin
 	for i in 0 to 11 loop
 		if RRI(i) = '1' then
