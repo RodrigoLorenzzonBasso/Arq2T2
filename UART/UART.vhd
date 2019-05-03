daa9497b0797e7f5d173e2378fa9536b0162c71a
Rodrigo Basso <rodrigolorenzzonbasso@gmail.com>
2019-05-03 17:29:41 -0300

    Tentei

--- UART/UART.vhd
+++ UART/UART.vhd
@@ -31,11 +31,14 @@ end UART;
 
 architecture UART of UART is
 
-	signal RW: std_logic_vector(7 downto 0) := "00000000";
-	signal BASE: std_logic_vector(7 downto 0) := "00000000";
+	signal RW: std_logic_vector(7 downto 0) := "ZZZZZZZZ";
+	signal BASE: std_logic_vector(7 downto 0) := "11111111";
 	signal RR: std_logic_vector(7 downto 0) := "00000000";
-	signal RRI: std_logic_vector(11 downto 0) := "000000000000";
+	signal RRI: std_logic_vector(11 downto 0) := "000000000011";
 	signal RWO: std_logic_vector(11 downto 0) := "000000000000";
+	
+	signal esperandoRespostaCPU: std_logic := '0';
+	signal esperandoRespostaCPU2: std_logic := '0';
 	
 	signal temDadoParaTransferir: std_logic := '0';
 	signal transferindo: std_logic := '0';
@@ -45,17 +48,25 @@ architecture UART of UART is
 	signal recebendo: std_logic := '0';
 	signal contadorRX: integer := 11;
 	
+	signal umaVezTX: std_logic := '0';
+	signal umaVezRX: std_logic := '0';
+	
+	signal interrupcao: std_logic := '0';
+	signal interrupcao2: std_logic;
+	
 
 
 begin
 
-	data(31 downto 0) <= x"000000" & BASE when ce = '1' and address = x"FFE0000" else
-								x"000000" & RW when ce = '1' and address = x"FFE0004" else
-								x"000000" & RR when ce = '1' and address = x"FFE0008" else
+	data(31 downto 0) <= x"000000" & BASE when ce = '1' and address = x"FFE00000" else
+								x"000000" & RR when ce = '1' and address = x"FFE00008" else
 								(others => 'Z');
+	
+	RW <= data(7 downto 0) when ce = '1' and address = x"FFE00004" else "ZZZZZZZZ";
 								
-	temDadoParaTransferir <= '1' when RWO(11) = '0' and RWO(1 downto 0) = "11" and temDadoParaTransferir = '0' else '0';
 	temDadoParaReceber <= '1' when RX = '0' and recebendo = '0' else '0';
+	
+	intr <= interrupcao or interrupcao2;
 								
 	-- Transmissao
 	
@@ -64,9 +75,27 @@ begin
 	
 		if rst = '1' then
 		
+			TX <= '0';
+			contadorTX <= 11;
+			sendTX <= '0';
+			transferindo <= '0';
+			interrupcao <= '0';
+			ackRX <= '0';
+			contadorRX <= 11;
+			recebendo <= '0';
+			esperandoRespostaCPU <= '0';
+			interrupcao2 <= '0';
+			esperandoRespostaCPU2 <= '0';
+			
 		elsif ck'event and ck = '1' then
 			
 			--------
+			
+			if RWO(11) = '0' and RWO(1 downto 0) = "11" then
+				temDadoParaTransferir <= '1';
+			else
+				temDadoParaTransferir <= '0';
+			end if;
 			
 			if transferindo = '1' then
 			
@@ -76,11 +105,13 @@ begin
 					
 						TX <= RWO(contadorTX);
 						sendTX <= '1';
+						umaVezTX <= '1';
 				
-					elsif ackTX = '1' then
+					elsif ackTX = '1' and umaVezTX = '1' then
 					
-						if sendTX = '1' then
+						if umaVezTX = '1' then
 							contadorTX <= contadorTX - 1;
+							umaVezTX <= '0';
 						end if;
 						sendTX <= '0';
 					
@@ -100,35 +131,97 @@ begin
 			
 				temDadoParaTransferir <= '0';
 				transferindo <= '1';
-				contador <= 11;
+				contadorTX <= 11;
 			
 			end if;
 			
-		end if;
-	
-	end process;
-	
-	-- Recepcao
-	
-	process(ck, rst)
-	begin
-	
-		if rst = '1' then
-		
-		elsif ck'event and ck = '1' then	
-		
+			if esperandoRespostaCPU2 = '0' then
 			
+				if RW = "ZZZZZZZZ" then
 			
-			
-			if temDadoParaReceber = '1' and recebendo = '0' and RRI(1 downto 0) = "11" then
-			
-				recebendo <= '1';
-				contadorRX <= 11;
+					if interrupcao2 <= '0' and BASE = "11111111" then
+						interrupcao2 <= '1';
+						BASE <= "00000000";
+						esperandoRespostaCPU2 <= '1';
+					--elsif interrupcao <= '1' then
+						-- TODO COLOCAR NA FILA DE INTERRUPCOES
+					end if;
+					
+				end if;
 				
+			elsif esperandoRespostaCPU2 = '1' then
+				
+				if inta = '1' then
+				
+					esperandoRespostaCPU2 <= '0';
+					interrupcao2 <= '0';
+					BASE <= "11111111";
+				
+				end if;
+			
 			end if;
+			
+			-------------------------------------------- RECEBENDO DADOS
 		
+			if esperandoRespostaCPU = '0' then
+				if recebendo = '1' then
+				
+					if contadorRX >= 0 then
+					
+						if sendRX = '1' then
+						
+							RRI(contadorRX) <= RX;
+							ackRX <= '1';
+							umaVezRX <= '1';
+						
+						elsif sendRX = '0' and umaVezRX = '1' then
+						
+							if umaVezRX = '1' then
+								contadorRX <= contadorRX - 1;
+								umaVezRX <= '0';
+							end if;
+							ackRX <= '0';
+						
+						end if;
+					
+					else
+						
+						recebendo <= '0';
+						RR <= RRI(9 downto 2);
+						----- aciona interrupcao
+						if interrupcao = '0' and BASE = "11111111" then
+							interrupcao <= '1';
+							BASE <= "00000001";
+							esperandoRespostaCPU <= '1';
+						--elsif interrupcao = '1' then
+							--TODO CRIAR FILA DE INTERRUPÇOES
+						end if;
+						
+					end if;
+					
+				---------
+				
+				elsif temDadoParaReceber = '1' and recebendo = '0' and RRI(1 downto 0) = "11" then
+				
+					recebendo <= '1';
+					contadorRX <= 11;
+					
+				end if;
+			elsif esperandoRespostaCPU = '1' then
+				if inta <= '1' then
+					interrupcao <= '0';
+					esperandoRespostaCPU <= '0';
+					BASE <= "11111111";
+				end if;
+			end if;
+			
+			if RW /= "ZZZZZZZZ"  then
+		
+				RWO <= "00" & RW & "11";
+		
+			end if;
+			
 		end if;
-	
 	
 	end process;
 			
